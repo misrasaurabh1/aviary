f664543a6c8d1733ebf194927cf817a7b02f6f83
James Braza <jamesbraza@gmail.com>
2024-09-10 17:50:15 -0700

    `ToolSelector` supporting being passed an `acompletion` (#26)

--- src/aviary/tools/utils.py
+++ src/aviary/tools/utils.py
@@ -1,4 +1,6 @@
+from collections.abc import Callable
 from functools import partial
+from typing import TYPE_CHECKING, cast
 
 from pydantic import BaseModel, Field
 
@@ -12,25 +14,44 @@ from .base import (
     ToolsAdapter,
 )
 
+if TYPE_CHECKING:
+    from collections.abc import Awaitable
+
+    from litellm import ModelResponse
+
 
 class ToolSelector:
     """Simple entity to select a tool based on messages."""
 
-    def __init__(self, model: str = "gpt-4o"):
-        try:
-            from litellm import acompletion
-        except ImportError as e:
-            raise ImportError(
-                f"{type(self).__name__} requires the 'llm' extra for 'litellm'. Please:"
-                " `pip install aviary[llm]`."
-            ) from e
-        self._acompletion = partial(acompletion, model)
+    def __init__(
+        self,
+        model_name: str = "gpt-4o",
+        acompletion: "Callable[..., Awaitable[ModelResponse]] | None" = None,
+    ):
+        """Initialize.
+
+        Args:
+            model_name: Name of the model to select a tool with.
+            acompletion: Optional async completion function to use, leaving as the
+                default of None will use LiteLLM's acompletion. Alternately, specify
+                LiteLLM's Router.acompletion function for centralized rate limiting.
+        """
+        if acompletion is None:
+            try:
+                from litellm import acompletion
+            except ImportError as e:
+                raise ImportError(
+                    f"{type(self).__name__} requires the 'llm' extra for 'litellm'. Please:"
+                    " `pip install aviary[llm]`."
+                ) from e
+
+        self._bound_acompletion = partial(cast(Callable, acompletion), model_name)
 
     async def __call__(
         self, messages: list[Message], tools: list[Tool]
     ) -> ToolRequestMessage:
         """Run a completion that selects a tool in tools given the messages."""
-        model_response = await self._acompletion(
+        model_response = await self._bound_acompletion(
             messages=MessagesAdapter.dump_python(
                 messages, exclude_none=True, by_alias=True
             ),
--- tests/test_envs.py
+++ tests/test_envs.py
@@ -4,6 +4,7 @@ import pathlib
 import tempfile
 from typing import ClassVar
 
+import litellm
 import pytest
 from pydantic import BaseModel
 
@@ -328,7 +329,7 @@ class TestParallelism:
 
     @pytest.mark.parametrize("model_name", [CILLMModelNames.OPENAI.value])
     @pytest.mark.asyncio
-    async def test_with_tool_selector(self, model_name: str) -> None:
+    async def test_tool_selector_from_model_name(self, model_name: str) -> None:
         env = ParallelizedDummyEnv()
         obs, tools = await env.reset()
         ledger = ToolSelectorLedger(tools=tools)
@@ -337,5 +338,25 @@ class TestParallelism:
         tool_request_message = await selector(obs, tools)
         ledger.messages.append(tool_request_message)
         ledger.model_dump()  # Proving we can serialize the ledger
+        assert isinstance(tool_request_message, ToolRequestMessage)
+        assert tool_request_message.tool_calls, "Expected at least one tool call"
+
+    @pytest.mark.parametrize("model_name", [CILLMModelNames.OPENAI.value])
+    @pytest.mark.asyncio
+    async def test_tool_selector_with_external_acompletion(
+        self, model_name: str
+    ) -> None:
+        env = ParallelizedDummyEnv()
+        obs_tools = await env.reset()
+
+        router = litellm.Router(
+            model_list=[
+                litellm.DeploymentTypedDict(
+                    model_name="openai", litellm_params={"model": model_name}
+                )
+            ]
+        )
+        selector = ToolSelector("openai", router.acompletion)
+        tool_request_message = await selector(*obs_tools)
         assert isinstance(tool_request_message, ToolRequestMessage)
         assert tool_request_message.tool_calls, "Expected at least one tool call"
