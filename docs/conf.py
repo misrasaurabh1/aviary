388bd136d6543a26dcf7a84d5d0ed289da5d0bb7
Manu Ponnapati <manu@Manus-MacBook-Pro-2.local>
2024-10-25 22:23:21 -0700

    save changes

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src/aviary'))
+
+project = 'aviary'
+copyright = '2024, FutureHouse'
+author = 'FutureHouse'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+]
+
+autosummary_generate = True
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/modules.rst
@@ -0,0 +1,14 @@
+aviary
+======
+
+.. toctree::
+   :maxdepth: 4
+
+   core
+   env
+   env_client
+   main
+   message
+   render
+   tools
+   utils
