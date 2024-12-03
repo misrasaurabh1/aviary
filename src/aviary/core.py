4381ee3733337e97a02bc8f001a7bb085acb5def
Andrew White <white.d.andrew@gmail.com>
2024-12-02 16:45:04 -0800

    Implements new functional syntax (#132)
    Per discussion on slack, new interface for defining environments

--- src/aviary/core.py
+++ src/aviary/core.py
@@ -15,6 +15,7 @@ from aviary.env_client import (
     TaskEnvClientState,
     TaskEnvironmentClient,
 )
+from aviary.functional import DynamicState, fenv
 from aviary.message import EnvStateMessage, MalformedMessageError, Message, join
 from aviary.render import Renderer
 from aviary.tools import (
@@ -45,6 +46,7 @@ __all__ = [
     "DummyEnv",
     "DummyEnvState",
     "DummyTaskDataset",
+    "DynamicState",
     "EnvStateMessage",
     "Environment",
     "EnvironmentClient",
@@ -75,6 +77,7 @@ __all__ = [
     "argref_by_name",
     "encode_image_to_base64",
     "eval_answer",
+    "fenv",
     "is_coroutine_callable",
     "join",
     "partial_format",
